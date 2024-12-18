4eed37a69ff222ca819eb4632e04a7c652c8b38a
Eszti2222 <nagy.eszter2222@gmail.com>
2024-12-18 13:21:06 +0100

    jatekos osztaly

--- Jatekos.py
+++ Jatekos.py
@@ -1,1 +1,12 @@
 class Jatekos():
+    def __init__(self, kredit, aru, dokkolo, tolmacsgep, kontener, tank, pozicio ):
+        self.kredit=kredit
+        self.aru=aru
+        self.dokkolo=dokkolo
+        self.tolmacsgep=tolmacsgep
+        self.kontener=kontener
+        self.tank=tank
+        self.pozicio=pozicio
+
+jatekos= Jatekos(10,0,0, False, False,0,1,"Thorodin")
+
