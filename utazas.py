ca4aea9547a7a17ee88ee853c141c99acbde174e
Eszti2222 <nagy.eszter2222@gmail.com>
2024-12-18 13:50:50 +0100

    utazas

---
+++ utazas.py
@@ -0,0 +1,16 @@
+from Jatekos import jatekos
+
+def hova_utazzunk(jatekos):
+    print(f"Hova utazzunk?\n")
+    uticel:str=input(f"Ydalir bolygó ='a'  || Vidar bolygó ='b'")
+
+
+    if(jatekos.pozicio, uticel="a"):
+        set jatekos.pozicio="Ydalir"
+    elif(uticel="b"):
+        set jatekos.pozicio="Vidar"
+    else:
+        get_pozicio()
+
+
+
